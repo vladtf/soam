58debe7770586c8e5d7fc33b6a0d249be7c80ed8
vtiftilov <vlad.tiftilov@yahoo.com>
2025-08-04 20:28:32 +0300

    fix: resolve Leaflet default marker icons issue in MapPage component

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 import { Container } from 'react-bootstrap';
 import NewBuildingModal from '../components/NewBuildingModal';
 import { postNewBuilding, fetchBuildings } from '../api/backendRequests';
 import { Building } from '../models/Building';
+
+// Fix Leaflet default marker icons issue in React
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 
 
